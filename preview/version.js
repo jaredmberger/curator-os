6dffaf7396b4a107f32c19ac2b7d22ec7a0e5de7
Jared Berger <jaredmberger@gmail.com>
2026-08-07 15:41:41 -0500

    Bump CuratorOS to v0.2.0 for Cloudflare Pages migration

--- preview/version.js
+++ preview/version.js
@@ -1,6 +1,6 @@
 // Canonical CuratorOS application version.
 // Bump this value whenever a user-visible CuratorOS release is deployed.
-export const CURATOROS_VERSION = '0.1.0';
+export const CURATOROS_VERSION = '0.2.0';
 
 window.CURATOROS_VERSION = CURATOROS_VERSION;
 document.documentElement.dataset.curatorosVersion = CURATOROS_VERSION;
